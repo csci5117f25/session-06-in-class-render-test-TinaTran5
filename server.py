611c4cba7e1f2198f8920dcb354de5ab2e4c1178
TinaTran5 <tinathytran5@gmail.com>
2025-10-01 23:01:52 -0500

    purecss holy grail layout

--- server.py
+++ server.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import db
+import os
 
 app = Flask(__name__)
+app.secret_key = os.environ['FLASK_SECRET']
 db.setup()
 
 
 @app.route("/")
 @app.route("/<name>")
 def hello(name=None):
-    return render_template("hello.html", name=name, guestbook=db.get_guestbook())
+    if name is not None: 
+        session["name"] = name
+    return render_template(
+        "hello.html", name=session.get("name"), guestbook=db.get_guestbook()
+    )
+
 
 
 @app.post("/submit")
